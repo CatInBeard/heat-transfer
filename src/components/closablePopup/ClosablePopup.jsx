024737517b8651b37c38619e10e188ae1ff7a153
Grigoriy Efimov <efimov-gm@newpage.xyz>
2023-11-05 19:26:58 +0300

    Improve closable popup: add escape key handling

--- src/components/closablePopup/ClosablePopup.jsx
+++ src/components/closablePopup/ClosablePopup.jsx
@@ -1,7 +1,21 @@
 import PopupContainer from "../popup/PopupContainer";
 import s from "./ClosablePopup.module.css"
+import { useEffect } from "react";
 
 let ClosablePopupContainer = ({children,cancelAction, headerText="Warning"}) => {
+    useEffect(() => {
+    const handleKeydown = (event) => {
+        if (event.key === 'Escape') {
+            cancelAction()
+        }
+    };
+
+    document.addEventListener('keydown', handleKeydown);
+
+    return () => {
+        document.removeEventListener('keydown', handleKeydown);
+    };
+    }, []);
     return <PopupContainer>
             <div className={"toast show " + s.confirm} role="alert" aria-live="assertive" aria-atomic="true">
                 <div className="toast-header">
