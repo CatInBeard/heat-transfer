713828c477ae71a22e210804e5a5c4c83f334ab4
Grigoriy Efimov <efimov-gm@newpage.xyz>
2024-05-11 01:02:44 +0300

    fix deploy 3

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,12 @@
 # Deploy production script
 # Triggering by main push
 
+
+repositoryOwner=$(jq -r '.repository.owner.name' "$GITHUB_EVENT_PATH")
+repository=$(jq -r '.repository.name' "$GITHUB_EVENT_PATH")
+
+echo "https://$repositoryOwner.github.io/$repository/" >> package.json
+
 mkdir -p public_html
 docker run --rm  -v $PWD:/usr/src/app -w /usr/src/app node:18-alpine npm install
 docker run --rm -v $PWD/public_html:/usr/src/app/build -v $PWD:/usr/src/app -w /usr/src/app node:18-alpine npm run build
